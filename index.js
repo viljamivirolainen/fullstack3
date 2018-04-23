37699964422fee5b99b6cbcd6c13e456c710e65b
Viljami <viljami.virolainen@aalto.fi>
2018-04-23 18:33:40 +0300

    3.6 done'd

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+app.use(bodyParser.json())
+morgan.token('content', function (req, res) { 
+  const body = req.body || {}
+  return JSON.stringify(body) 
+})
+app.use(morgan(':method :url :content :status :res[content-length] - :response-time ms'
+))
+
 
 let persons = [
       {
@@ -43,13 +53,44 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
-  
 })
 
 app.get('/info', (req, res) => {
   const sum = persons.length
   const date = new Date() 
   res.send('<p>puhelinluettelossa ' + sum + ' ihmisen tiedot<p><p>'+ date +'</p>')
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons/', (req, res) => {
+  const person = req.body
+  const name = person.name
+  const number = person.number
+  if(!person) {
+    res.status(400).json({error: 'no person data received'})
+  }
+  if(!name) {
+    res.status(400).json({error: 'name missing'})
+  }
+  if(!number) {
+    res.status(400).json({error: 'number missing'})
+  }
+
+  if(persons.map(person=>person.name).includes(name)) {
+    res.status(400).json({error: 'name must be unique'})
+  }
+
+
+
+  const id = Math.floor(Math.random()*Math.floor(1000000000))
+  const personWithId = {name:name, number:number,id:id}
+  persons.push(personWithId)
+  res.json(personWithId)
 })
 
 const PORT = 3001
