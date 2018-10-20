31b261c2306d9c829d34f69f9ef6a91e47bd8c7d
Viljami Virolainen <viljami.virolainen@aalto.fi>
2018-10-20 17:01:34 +0300

    added build of frontend and configured backend to use it

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('build'))
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 app.use(bodyParser.json())
